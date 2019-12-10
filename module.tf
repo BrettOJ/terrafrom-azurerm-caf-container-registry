2e105ede33f76accfc507f3cd7c6c557f297b35b
Brett Odgers-Jewell <brett.odgers-jewell@avanade.com>
2019-12-10 10:54:29 +0800

    updated Module

--- module.tf
+++ module.tf
@@ -2,8 +2,8 @@
 resource "azurerm_container_registry" "acr" {
   
   name                     = var.container_registry_name
-  resource_group_name      = azurerm_resource_group.rg.name
-  location                 = azurerm_resource_group.rg.location
+  resource_group_name      = var.resource_group_name
+  location                 = var.location
   sku                      = var.sku
   admin_enabled            = var.admin_enabled
   #georeplication_locations = ["East US", "West Europe"]
--- variables.tf
+++ variables.tf
@@ -1,6 +1,14 @@
-
+variable "resource_group_name" {
+  description = "(Required) Name of the resource group to deploy the activity logs."
+}
 variable "location" {
-  description = "location of the variable group"
+  description = "(Required) Define the region where the resources will be created."
+}
+variable "tags" {
+  description = "(Required) Tags for the logs repositories to be created "
+ }
+variable "prefix" {
+  description = "(Optional) You can use a prefix to add to the list of resource groups you want to create"
 }
 variable "sku" {
   description = "Sku of the container registry"
@@ -9,5 +17,4 @@ variable "container_registry_name" {
   description = "Name of the container registry"
 }
 variable "admin_enabled" {
-  
 }
